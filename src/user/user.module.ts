cbf8fc4036aed2b0ca77e83ef35201ae9bfb7bd5
valentimdev <valentimdesenvolvedor@gmail.com>
2025-11-18 12:20:50 -0300

    feat: adicionando url no editUserDto

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { BookModule } from 'src/book/book.module';
 
 @Module({
+  imports: [BookModule],
   controllers: [UserController],
   providers: [UserService]
 })

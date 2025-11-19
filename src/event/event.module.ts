0ae7ebc82a44cd655444b338d7c7244e266988a9
Guilherme <66035579+guigasdev@users.noreply.github.com>
2025-11-19 09:20:43 -0300

    feat: criando notificação

--- src/event/event.module.ts
+++ src/event/event.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { EventService } from './event.service';
 import { EventController } from './event.controller';
 import { StorageModule } from '../storage/storage.module';
+import { NotificationModule } from 'src/notification/notification.module';
 
 @Module({
-  imports: [StorageModule],
+  imports: [StorageModule, NotificationModule],
   controllers: [EventController],
   providers: [EventService],
   exports: [EventService],
---
+++ src/notification/dto/create-notification.dto.ts
@@ -0,0 +1,31 @@
+import { NotificationType } from '@prisma/client';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateNotificationDto {
+  @IsString()
+  @IsNotEmpty()
+  userId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(120)
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  message: string;
+
+  @IsEnum(NotificationType)
+  @IsOptional()
+  type?: NotificationType;
+
+  @IsOptional()
+  data?: Record<string, unknown>;
+}
+

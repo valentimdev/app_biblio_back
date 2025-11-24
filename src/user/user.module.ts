a4f0e056949086b5b213c2d1da92735c3790a073
valentimdev <valentimdesenvolvedor@gmail.com>
2025-11-24 12:38:49 -0300

    fix:dependencias no rental

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -7,6 +7,7 @@ import { StorageModule } from 'src/storage/storage.module';
 @Module({
   imports: [BookModule, EventModule, StorageModule],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
